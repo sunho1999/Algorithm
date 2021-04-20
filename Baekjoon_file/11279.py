bfb7260a0678adca277846aef7d7f16d29417979
sunho1999 <wmfrlek11@naver.com>
2021-04-20 21:16:39 +0900

    update 11047.py 2021-04-20

--- Baekjoon_file/11279.py
+++ Baekjoon_file/11279.py
@@ -1,19 +1,11 @@
 import heapq
 import sys
-
-<<<<<<< HEAD
 N = int(sys.stdin.readline())
 
 heap = []
 
 for i in range(N):
-=======
-n = int(sys.stdin.readline())
 
-heap = []
-
-for i in range(n):
->>>>>>> 902afb7ae9b2b2c8ac36873864403c6ce48f2d1f
     a = int(sys.stdin.readline())
     if a == 0:
         if heap:
