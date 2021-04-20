d8216ed63f0efae61efae4c0862de5149971792a
sunho1999 <wmfrlek11@naver.com>
2021-04-20 21:27:39 +0900

    11407.py update 2021-04-20

---
+++ Baekjoon_file/11047.py
@@ -0,0 +1,14 @@
+n , goal = map(int,input().split())
+coin_list =[]
+sum = 0
+#동전 저장
+for i in range(n):
+    coin_list.append(int(input()))
+max = 0
+for i in range(n-1,-1,-1):
+    if coin_list[i] > goal:
+        continue
+    else:
+        sum = sum + goal//coin_list[i]
+        goal = goal%coin_list[i]
+print(sum)
