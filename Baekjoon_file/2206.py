d696b954e64bd4f3301dcaf9e3b783b2604218b0
sunho1999 <wmfrlek11@naver.com>
2021-05-23 19:28:23 +0900

    update 2206.py

---
+++ Baekjoon_file/2206.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+
+n ,m = map(int,input().split())
+matrix =[]
+for i in range(n):
+    matrix.append(list(map(int,sys.stdin.readline().strip())))
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs():
+    q = deque()
+    q.append([0,0,1])
+    visit = [[[0] * 2 for i in range(m)] for i in range(n)]
+    visit[0][0][1] = 1
+    while q:
+        a,b,w = q.popleft()
+        if a == n-1 and b == m-1:
+            return visit[a][b][w]
+        for i in range(4):
+            x = a + dx[i]
+            y = b + dy[i]
+            if 0 <= x < n and 0 <= y < m:
+                if matrix[x][y] == 1 and w == 1:
+                    visit[x][y][0] = visit[a][b][1] + 1
+                    q.append([x,y,0])
+                elif matrix[x][y] == 0 and visit[x][y][w] == 0:
+                    visit[x][y][w] = visit[a][b][w] + 1
+                    q.append([x, y, w])
+    return -1
+
+
+print(bfs())
--- Baekjoon_file/test.py
+++
@@ -1,7 +0,0 @@
-
-a = 3
-
-for i in range(3,4):
-    for j in range(4,5):
-        print(i)
-        print(j)
