28b21904a8d653f33e84fe8eb9cd3b6fd359b8ce
sunho1999 <wmfrlek11@naver.com>
2021-06-11 20:23:11 +0900

    update 1309.py 21-06-11

---
+++ Baekjoon_file/1309.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+dp = [1 for i in range(n+1)]
+
+dp[1] = 3
+
+if n == 1:
+    print(3)
+else:
+    for i in range(2,n+1):
+        dp[i] =  2*dp[i-1] + dp[i-2]
+        dp[i] = dp[i]%9901
+
+    print(dp[n])
