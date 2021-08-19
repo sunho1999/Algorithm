e8cccf66f19512ff1c16ae7849946f44ad8f3cbb
sunho1999 <wmfrlek11@naver.com>
2021-08-19 20:54:45 +0900

    update 16724.py 21-08-19

--- Baekjoon_file/16724.py
+++ Baekjoon_file/16724.py
@@ -6,6 +6,7 @@ n,m = map(int,input().split())
 matrix = [list(input().strip()) for _ in range(n)]
 visited =[[0 for _ in range(m)] for _ in range(n)]
 
+check = 1
 def D(y,x):
     y = y + 1
     x = x
@@ -23,13 +24,18 @@ def U(y,x):
     y = y - 1
     return (y, x)
 def dfs(y,x):
+    global cnt
+    global check
     que = deque()
     que.append([y,x])
-
+    save =  [y, x] # 첫 방문 좌표 저장
     while que:
+
         y,x = que.popleft()
+        # 첫 방문노드 도착했을 때
         if visited[y][x] == 0:
-            visited[y][x] = 1
+            #방문노드 저장
+            visited[y][x] = check
             if matrix[y][x] == "D":
                 y,x = D(y,x)
                 que.append([y,x])
@@ -42,15 +48,18 @@ def dfs(y,x):
             elif matrix[y][x] == "L":
                 y,x = L(y,x)
                 que.append([y,x])
-    else:
-        return
 
+        elif visited[y][x] == check:
+            # 다시 싸이클을 돌아 처음 방문했던 좌표에 도착했을 때
+            cnt +=1
+            return
+            # 싸이클을 돌았는데 처음 시작한 싸이클이 아니고 다른 싸이클에 도착했을 때
+        elif visited[y][x] != check:
+            return
 
 for i in range(n):
     for j in range(m):
         if visited[i][j] == 0:
             dfs(i,j)
-            print(visited)
-            cnt +=1
-
+            check +=1
 print(cnt)
