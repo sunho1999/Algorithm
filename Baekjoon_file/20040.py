f6abb604c5d60275775545d410863aa90cd7fb03
sunho1999 <wmfrlek11@naver.com>
2021-08-22 19:44:39 +0900

    update 20040.py 21-08-22

--- Baekjoon_file/20040.py
+++ Baekjoon_file/20040.py
@@ -1,40 +1,28 @@
-from collections import deque
+from sys import stdin
+input = stdin.readline
+import sys
+sys.setrecursionlimit(200000)
+def find(x):
+    if x == arr[x]:
+        return x
+    else:
+        arr[x] = find(arr[x])
+        return arr[x]
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x < y:
+        arr[y] = x
+    else: arr[x] = y
 
-# 노드 개수, 간선 개수
-n,m = map(int,input().split())
+N, M = map(int, input().split())
+arr = [i for i in range(N)]
+for i in range(M):
+    a, b = map(int, input().split())
+    if find(a) == find(b):
+        print(i+1)
+        break
+    union(a, b)
 
-# 인접 리스트
-matrix = [[] for _ in range(n)]
-# 방문 노드 저장
-visited = [0 for _ in range(n)]
-
-
-def dfs(b,save):
-    # 시작 노드 체크
-    que = deque()
-    que.append([b,save])
-    visited[b] = 1
-    while que:
-        node,save = que.popleft()
-        # 시작 좌표에서 연결된 간선 탐색
-        for i,save_node in matrix[node]:
-            # 방문한 좌표가 방문하지 않은 좌표일 때
-            if visited[i] == 0:
-                # 인접리스트의 간선 노드와 현재 간선노드가 같을 때 que에 추가
-                if matrix[i][save_node] == matrix[node][save]:
-                    que.append(i)
-            # 방문한 좌표가 방문했던 좌표일 때
-            if visited[i] != 0:
-
-
-
-save = 1
-for i in range(m):
-    a,b = map(int,input().split())
-    matrix[a].append([b,save])
-    matrix[b].append([a.save])
-    save +=1
-    dfs(b)
-print(matrix)
-
-
+else:
+    print(0)
