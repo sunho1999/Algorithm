882e1e0e8a28ed60e96d96707d0d0d9646b4ece9
sunho1999 <wmfrlek99@gmail.com>
2023-01-31 21:24:41 +0900

    1일 1알고리즘

---
+++ Baekjoon_file/2504.py
@@ -0,0 +1,50 @@
+import sys
+
+input = sys.stdin.readline
+
+s = input().strip()
+list1 = []
+answer = 0
+
+for i in s:
+    if i == ")":
+        t = 0
+        while len(list1 ) != 0:
+            top = list1 .pop()
+            if top == "(":
+                if t == 0:
+                    list1 .append(2)
+                else:
+                    list1 .append(2 * t)
+                break
+            elif top == "[":
+                print(0)
+                exit(0)
+            else:
+                t = t + int(top)
+    elif i == "]":
+        t = 0
+        while len(list1 ) != 0:
+            top = list1 .pop()
+            if top == "[":
+                if t == 0:
+                    list1 .append(3)
+                else:
+                    list1 .append(3 * t)
+                break
+            elif top == "(":
+                print(0)
+                exit(0)
+            else:
+                t = t + int(top)
+    else:
+        list1.append(i)
+
+for i in list1 :
+    if i == "(" or i == "[":
+        print(0)
+        exit(0)
+    else:
+        answer += i
+
+print(answer)
