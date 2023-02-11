2af404daa9ddfaf933b91494d84cb25d66c1a859
sunho1999 <wmfrlek99@gmail.com>
2023-02-11 17:36:51 +0900

    1일 1알고리즘

--- Baekjoon_file/9465.py
+++ Baekjoon_file/9465.py
@@ -6,10 +6,13 @@ for i in range(t):
     for j in range(2):
         s.append(list(map(int,input().split())))
     #기본 첫번째 인덱스 값 고정
-    s[0][1] = s[0][1]+s[1][0]
-    s[1][1] = s[1][1]+s[0][0]
-    for k in range(2,n):
-        s[0][k] = max(s[1][k-1],s[1][k-2]) + s[0][k]
-        s[1][k] = max(s[0][k-1],s[0][k-2]) + s[1][k]
+    if n == 1:
+        print(max(s[0][0],s[1][0]))
+    else:
+        s[0][1] = s[0][1]+s[1][0]
+        s[1][1] = s[1][1]+s[0][0]
+        for k in range(2,n):
+            s[0][k] = max(s[1][k-1],s[1][k-2]) + s[0][k]
+            s[1][k] = max(s[0][k-1],s[0][k-2]) + s[1][k]
 
-    print(max(s[0][n-1],s[1][n-1]))
+        print(max(s[0][n-1],s[1][n-1]))
