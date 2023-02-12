b0cb82c0ab3354330acdf63189d4a53a68a43008
sunho1999 <wmfrlek99@gmail.com>
2023-02-12 20:15:41 +0900

    1일 1알고리즘

---
+++ Baekjoon_file/1644.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+
+a = [False,False] + [True]*(n-1)
+primes=[]
+
+for i in range(2,n+1):
+  if a[i]:
+    primes.append(i)
+    for j in range(2*i, n+1, i):
+        a[j] = False
+answer = 0
+start = 0
+end = 0
+while end <= len(primes):
+    temp_sum = sum(primes[start:end])
+    if temp_sum == n:
+        answer += 1
+        end += 1
+    elif temp_sum < n:
+        end += 1
+    else:
+        start += 1
+print(answer)
