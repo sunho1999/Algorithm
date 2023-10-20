9634007668c2afc5f17121720d6e6dc101eefaa1
sunho1999 <wmfrlek99@gmail.com>
2023-10-20 22:29:05 +0900

    1일 1알고리즘

--- Baekjoon_file/2504.py
+++ Baekjoon_file/2504.py
@@ -3,19 +3,20 @@ import sys
 input = sys.stdin.readline
 
 s = input().strip()
+# print(s)
 list1 = []
 answer = 0
 
 for i in s:
     if i == ")":
         t = 0
-        while len(list1 ) != 0:
-            top = list1 .pop()
+        while len(list1) != 0:
+            top = list1.pop()
             if top == "(":
                 if t == 0:
-                    list1 .append(2)
+                    list1.append(2)
                 else:
-                    list1 .append(2 * t)
+                    list1.append(2 * t)
                 break
             elif top == "[":
                 print(0)
