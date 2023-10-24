ee0a7068dff1a442a98e04edd849f7f32b9e8411
sunho1999 <wmfrlek99@gmail.com>
2023-10-24 12:31:55 +0900

    1일 1알고리즘

--- Baekjoon_file/11660.py
+++ Baekjoon_file/11660.py
@@ -15,12 +15,11 @@ for i in range(1, n + 1):
     for j in range(1, n):
         list1[i][j + 1] += list1[i][j]
 
+
 # 열 별로 더하기
 for j in range(1, n + 1):
     for i in range(1, n):
         list1[i + 1][j] += list1[i][j]
-
-
 for _ in range(m):
     x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
     print(list1[x2][y2] - list1[x1 - 1][y2] - list1[x2][y1 - 1] + list1[x1 - 1][y1 - 1])
