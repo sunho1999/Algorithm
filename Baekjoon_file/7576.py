67e198303284e8d681d5f1958ee3eb6b2866a1c0
sunho1999 <wmfrlek99@gmail.com>
2023-10-25 12:29:23 +0900

    1일 1알고리즘

--- Baekjoon_file/7576.py
+++ Baekjoon_file/7576.py
@@ -1,50 +1,41 @@
 import sys
 from collections import deque
-m,n = map(int,sys.stdin.readline().split())
-#토마토 상자
+
+input = sys.stdin.readline
+
+M,N = map(int,input().split()) #가로, 세로
+
+visited = [[0 for _ in range(M)] for _ in range(N)]
+matrix = [list(map(int,input().split())) for _ in range(N)]
+
 que = deque()
-matrix = []
-for i in range(n):
-    matrix.append(list(map(int,sys.stdin.readline().split())))
 
-
-dx = [1,-1,0,0]
-dy = [0,0,-1,1]
-#1이 있는 좌표 que에 담기
-for i in range(n):
-    for j in range(m):
+for i in range(N): #세로
+    for j in range(M):  #가로
         if matrix[i][j] == 1:
-            que.append([i,j])
+            que.append((i,j))
 
-#bfs로 1에 있는 인접좌표 0이면 바꾸기 바꾸면서 day +=1
-def bfs():
-    day = -1
-    while que:
-        day+=1
-        a, b = que.popleft()
-        for i in range(4):
-            x = dx[i] + a
-            y = dy[i] + b
-            if (0 <= x < n) and (0 <= y < m) and (matrix[x][y] == 0):
-                matrix[x][y] = matrix[a][b]+1
-                que.append([x,y])
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-bfs()
-isTrue = False
-result = -2
-#첫번쨰줄 상자 부터 접근해서 맥스 값 따오기
-for i in matrix:
-    for j in i:
-        if j == 0:
-            isTrue = True
-        result = max(result, j)
-#상자 값중에 0이있으면 -1 출력
-if isTrue == True:
-    print(-1)
-#상자 값중에 1이 최대값이면 0출력
-elif result == -1:
-    print(0)
-#상자 값 중에 제일 높은값 -1 출력 > 기존 1이 담겨있어서 카운트를 하면 안됨.
-else:
-    print(result - 1)
+cnt = 1
+while que:
+    x,y = que.popleft()
+    for i in range(4):
+        ax = x + dx[i]
+        ay = y + dy[i]
+        if 0 <= ax < N  and 0<= ay < M:
+            if matrix[ax][ay] == 0 :
+                matrix[ax][ay] = matrix[x][y] + 1
+                que.append((ax,ay))
 
+max_value = 0
+for i in range(N):
+    for j in range(M):
+        if matrix[i][j] == 0:
+            print(-1)
+            exit()
+    if max(matrix[i]) > max_value:
+        max_value = max(matrix[i])
+
+print(max_value-1)
