5ef121cab637f0d8ab2e7ff667cae622d0025a25
sunho1999 <wmfrlek99@gmail.com>
2024-01-23 13:09:02 +0900

    코테 준비

---
+++ Baekjoon_file/20922.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+import sys
+input= sys.stdin.readline
+n,k = map(int,input().split())
+li = list(map(int,input().split()))
+left, right = 0, 0
+dd = defaultdict(int)
+ans = 0
+
+while True:
+    if right == n:
+        break
+    if dd[li[right]] < k:
+        dd[li[right]] +=1
+        right +=1
+    else:
+        dd[li[left]] -=1
+        left +=1
+    ans = max(right - left, ans)
+print(ans)
