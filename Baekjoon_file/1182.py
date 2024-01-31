5f826affe821e6aea522a51874780bee52756bd3
sunho1999 <wmfrlek99@gmail.com>
2024-01-31 15:25:27 +0900

    코테준비

--- Baekjoon_file/1182.py
+++ Baekjoon_file/1182.py
@@ -1,14 +1,26 @@
-n,m = map(int,input().split())
+import sys
 
-num_list = list(map(int,input().split())) # -7,-3,-2,5,8
-
+input = sys.stdin.readline
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
 cnt = 0
-for i in range(n-1):
-    dp = [0 for _ in range(n)]
-    dp[i] = num_list[i]
-    for j in range(i+1,n):
-        dp[j] = dp[j-1] + num_list[j]
-    if m in dp[i:]:
-        cnt +=1
-    print(dp)
+
+def subset_sum(idx, sub_sum):
+    global cnt
+
+    if idx >= n:
+        return
+
+    sub_sum += arr[idx]
+
+    if sub_sum == s:
+        cnt += 1
+
+    # 현재 arr[idx]를 선택한 경우의 가지
+    subset_sum(idx + 1, sub_sum)
+    # 현재 arr[idx]를 선택하지 않은 경우의 가지
+    subset_sum(idx + 1, sub_sum - arr[idx])
+
+
+subset_sum(0, 0)
 print(cnt)
