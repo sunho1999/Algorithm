8b83d2f269d5a9412d23b332569eef63553e3d33
sunho1999 <wmfrlek99@gmail.com>
2024-03-05 14:29:00 +0900

    코테준비

--- Baekjoon_file/2116.py
+++ Baekjoon_file/2116.py
@@ -1,39 +1,145 @@
-N = int(input())
-dice_list = [list(map(int, input().split())) for _ in range(N)]
-
-across_dict = {
-    0: 5,
-    1: 3,
-    2: 4,
-    3: 1,
-    4: 2,
-    5: 0
-}
-
-def cal_tot_cnt(num):
-    result = 0
-
-    for i in range(N):
-        for j in range(6):
-            if dice_list[i][j] == num:
-                across_num = dice_list[i][across_dict[j]]
-                if 6 in [num, across_num]:
-                    result += 4 if 5 in [num, across_num] else 5
-                else:
-                    result += 6
-                num = across_num
-                break
-
-    return result
+import copy
+import sys
+sys.setrecursionlimit(10**6)
 
 
-def solve():
-    result = 0
+n = int(input())
+n_dice = []
+total_dice = []
+depth =0
+summ_dice = 0
+for i in range(n):
+    n_dice.append(list(map(int,input().split())))
 
-    for i in range(1, 7):
-        result = max(result, cal_tot_cnt(i))
+def sum_dice(idx,depth):
+    global summ_dice
+    # print(idx,depth)
+    # print(summ_dice)
+    # print(test_dice)
+    if depth  >= n:
+        print("yes")
+        return summ_dice
+    if idx == 0:
+        new_idx = 5
+        start_idx = test_dice[depth][idx]
+        end_idx = test_dice[depth][new_idx]
+        if depth +1 >= n:
+            # print(depth)
+            test_dice[depth].remove(start_idx)
+            test_dice[depth].remove(end_idx)
+            max_num = max(test_dice[depth])
+            summ_dice += max_num
+            return summ_dice
+        target_idx = test_dice[depth+1].index(end_idx)
+        # print(target_idx)
+        test_dice[depth].remove(start_idx)
+        test_dice[depth].remove(end_idx)
+        max_num = max(test_dice[depth])
+        summ_dice += max_num
+        depth +=1
+        # print(n_dice)
+        # print(test_dice[depth].index(new_idx))
+        return sum_dice(target_idx,depth)
+    elif idx == 1:
+        new_idx = 3
+        start_idx = test_dice[depth][idx]
+        end_idx = test_dice[depth][new_idx]
+        if depth + 1 >= n:
+            # print(depth)
+            test_dice[depth].remove(start_idx)
+            test_dice[depth].remove(end_idx)
+            max_num = max(test_dice[depth])
+            summ_dice += max_num
+            return summ_dice
+        target_idx = test_dice[depth+1].index(end_idx)
+        test_dice[depth].remove(start_idx)
+        test_dice[depth].remove(end_idx)
+        max_num = max(test_dice[depth])
+        summ_dice += max_num
+        depth +=1
 
-    print(result)
+        return sum_dice(target_idx,depth)
+    elif idx == 2:
+        new_idx = 4
+        start_idx = test_dice[depth][idx]
+        end_idx = test_dice[depth][new_idx]
+        if depth + 1 >= n:
+            # print(depth)
+            test_dice[depth].remove(start_idx)
+            test_dice[depth].remove(end_idx)
+            max_num = max(test_dice[depth])
+            summ_dice += max_num
+            return summ_dice
+        target_idx = test_dice[depth+1].index(end_idx)
+        test_dice[depth].remove(start_idx)
+        test_dice[depth].remove(end_idx)
+        max_num = max(test_dice[depth])
+        summ_dice += max_num
+        depth +=1
 
+        return sum_dice(target_idx,depth)
+    elif idx == 3:
+        new_idx = 1
+        start_idx = test_dice[depth][idx]
+        end_idx = test_dice[depth][new_idx]
+        if depth + 1 >= n:
+            # print(depth)
+            test_dice[depth].remove(start_idx)
+            test_dice[depth].remove(end_idx)
+            max_num = max(test_dice[depth])
+            summ_dice += max_num
+            return summ_dice
+        target_idx = test_dice[depth+1].index(end_idx)
+        test_dice[depth].remove(start_idx)
+        test_dice[depth].remove(end_idx)
+        max_num = max(test_dice[depth])
+        summ_dice += max_num
+        depth +=1
 
-solve()
+        return sum_dice(target_idx,depth)
+    elif idx == 4:
+        new_idx = 2
+        start_idx = test_dice[depth][idx]
+        end_idx = test_dice[depth][new_idx]
+        if depth + 1 >= n:
+            # print(depth)
+            test_dice[depth].remove(start_idx)
+            test_dice[depth].remove(end_idx)
+            max_num = max(test_dice[depth])
+            summ_dice += max_num
+            return summ_dice
+        target_idx = test_dice[depth+1].index(end_idx)
+        test_dice[depth].remove(start_idx)
+        test_dice[depth].remove(end_idx)
+        max_num = max(test_dice[depth])
+        summ_dice += max_num
+        depth +=1
+
+        return sum_dice(target_idx,depth)
+    elif idx == 5:
+        new_idx = 0
+        start_idx = test_dice[depth][idx]
+        end_idx = test_dice[depth][new_idx]
+        if depth + 1 >= n:
+            # print(depth)
+            test_dice[depth].remove(start_idx)
+            test_dice[depth].remove(end_idx)
+            max_num = max(test_dice[depth])
+            summ_dice += max_num
+            return summ_dice
+        target_idx = test_dice[depth+1].index(end_idx)
+        test_dice[depth].remove(start_idx)
+        test_dice[depth].remove(end_idx)
+        max_num = max(test_dice[depth])
+        summ_dice += max_num
+        depth +=1
+
+        return sum_dice(target_idx,depth)
+for i in range(6):
+    test_dice = copy.deepcopy(n_dice)
+    depth = 0
+    summ_dice = 0
+    b = sum_dice(i,depth)
+    total_dice.append(b)
+# print(total_dice)
+print(max(total_dice))
