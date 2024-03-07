f398b13520bcb55881834e44345e1c08cb53d8ae
sunho1999 <wmfrlek99@gmail.com>
2024-03-07 16:44:53 +0900

    코드 수정

--- Baekjoon_file/5972.py
+++ Baekjoon_file/5972.py
@@ -1,22 +1,16 @@
-import sys
-
-input = sys.stdin.readline
 import heapq
 
+n,m = map(int,input().split())
 INF = int(1e9)
+linked_list = [[] for _ in range(n+1)]
 
-n, m = map(int, input().split())
+for i in range(m):
+    node1,node2,cost = map(int,input().split())
+    linked_list[node1].append((node2,cost))
+    linked_list[node2].append((node1,cost))
 
-graph = [[] for i in range(n + 1)]
-
-distance = [INF] * (n + 1)
-
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-    graph[b].append((a, c))
-
-
+distance = [INF] * (n+1)
+# print(distance)
 def dijkstra(start):
     q = []
     heapq.heappush(q, (0, start))
@@ -25,13 +19,16 @@ def dijkstra(start):
         dist, now = heapq.heappop(q)
         if distance[now] < dist:
             continue
-        for i in graph[now]:
+        for i in linked_list[now]:
             cost = dist + i[1]
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
 
-    return distance[n]
+dijkstra(1)
+# print(distance)
+if distance[n] == INF:
+    print(-1)
+else:
+    print(distance[n])
 
-
-print(dijkstra(1))
