9956dc6b9a6b9bdee63968271f141293d6598c5d
sunho1999 <wmfrlek99@gmail.com>
2024-06-26 15:51:02 +0900

    코테준비

---
+++ Baekjoon_file/1011.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+for i in range(t):
+    x, y = map(int, input().split())
+
+    distance = y - x  # y와 x사이의 거리
+    tmp = 0  # 이동 거리
+    cnt = 0  # 공간 장치 이동 횟수
+    moving = 0  # 반복 횟수
+
+    while tmp < distance:
+        cnt += 1
+        if cnt % 2 != 0:
+            moving += 1
+        tmp += moving
+    print(cnt)
