0860003730bc32af984b1ea896337618e34ee220
sunho1999 <wmfrlek99@gmail.com>
2025-03-07 19:23:52 +0900

    코테준비

--- Baekjoon_file/9466.py
+++ Baekjoon_file/9466.py
@@ -1,30 +1,25 @@
 import sys
+sys.setrecursionlimit(10**9)
+def dfs(start):
+    global answer
+    cycle.append(start)
+    check_num_list[start] = True
+    next_node = student_list[start]
 
-sys.setrecursionlimit(10**6)
-
-def dfs(n):
-    global count
-    visited[n] = True
-    cycle_list.append(n)
-    if visited[arr[n]] == True:
-        if arr[n] in cycle_list:
-            # print(cycle_list)
-            # print(cycle_list.index(arr[n]))
-            count -= len(cycle_list[cycle_list.index(arr[n]):])
+    if check_num_list[next_node]:  # 방문 가능 체크
+        if next_node in cycle:  # 사이클 가능
+            answer += cycle[cycle.index(next_node):]  # 사이클 구간 팀 이룸
         return
     else:
-        dfs(arr[n])
+        dfs(next_node)
+testcase = int(input())
 
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    arr = [0]
-    arr.extend([int(x) for x in sys.stdin.readline().rstrip().split()])
-    visited = [False] * (N + 1)
-    count = N
-    for i in range(1, N + 1):
-        if not visited[i]:
-            cycle_list = []
-            dfs(i)
-
-    print(count)
+for i in range(testcase):
+    answer = []
+    student_num = int(input())
+    student_list = [0] + list(map(int,input().split()))
+    check_num_list = [True] + [False for _ in range(student_num)]
+    for i in range(1,student_num+1):
+        cycle = []
+        dfs(i)
+    print(student_num - len(list(set(answer))))
